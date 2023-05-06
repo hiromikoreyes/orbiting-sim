890e59d72acc5891f5e784b719cabcb825405ee4
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2023-05-06 11:29:19 -0400

    saving :D (bug galore)

--- app.py
+++ app.py
@@ -10,16 +10,14 @@ clock = pygame.time.Clock()
 running = True
 
 #constants
-pi = 3.1415
-G = 0.06 #making it up
+
 
 
 #objects in simulation
-
-
-thingy = simulation.Body(10, (100, 100), (0,0), (0,0))
-thingy2 = simulation.Body(100, (1920//2,1080//2), (0,0), (0,0))
+thingy = simulation.Body(10, (1920//2 - 100,1080//2 - 100), (1,0), (0, 0))
+thingy2 = simulation.Body(1000, (1920//2,1080//2), (0,0), (0,0))
 objects = [thingy, thingy2]
+
 # for i in range(100):
 #     objects.append(simulation.Body(3, (i,i), (random.randint(-2,2), random.randint(-2,2)), (0,0)))
 
@@ -30,30 +28,33 @@ while running:
             running = False
 
     #logical stuff
-
     screen.fill("black")
 
     
 
     #rendering stuff
     for body in objects:
-        radius = np.cbrt(4 * body.mass / 3 * pi) 
+        radius = np.cbrt(4 * body.mass / 3 * np.pi) 
         pygame.draw.circle(screen, (255,255,255), (body.pos_x, body.pos_y), radius)
         
         #Update position each frame
-        body.pos_x += body.vel_x
-        body.pos_y += body.vel_y
+
 
         # For when we want wacky things to happen
-        # if not 0 < body.pos_x < 1920:
+        # if body.pos_x > 1920:
         #     body.pos_x = 0
-        # if not 0 < body.pos_y < 1920:
+        # elif body.pos_x < 0:
+        #     body.pos_x = 1920
+        # if body.pos_y > 1080:
         #     body.pos_y = 0
+        # elif body.pos_y < 0:
+        #     body.pos_y = 1080
+
 
         #orbiting physics calculation
         for body_alt in objects:
             if body_alt is not body:
-                body.gravity(body_alt)
+                body.gravity(screen, body_alt)
 
 
                     
@@ -61,6 +62,8 @@ while running:
         #collision calculation
 
         #Update velocity each frame
+        body.pos_x += body.vel_x
+        body.pos_y += body.vel_y
         body.vel_x += body.acc_x  
         body.vel_y += body.acc_y
         #we need a way to calculate acceleration :DDD
--- simulation.py
+++ simulation.py
@@ -1,3 +1,7 @@
+import numpy as np
+import pygame as pg 
+G = 0.0006 #making it up
+
 class Body:
     def __init__(self, mass: float, pos: tuple[float], vel: tuple[float], acc: tuple[float]):
         self.mass = mass
@@ -8,8 +12,58 @@ class Body:
         self.acc_x = acc[0]
         self.acc_y = acc[1]
 
-    def gravity(self, body_alt):
-        """ Calculates then adds force of gravity between two Bodies """
+    def collision(self, body_alt):
+        print("bodies collided!!!!")
+
+    def __get_angle(self, body_alt, displ_x, displ_y):
+        """ Returns the angle between two bodies, from the point of reference of self """
+
+        if(displ_x == 0 and displ_y > 0):
+            return np.pi / 2
+        elif(displ_x == 0 and displ_y < 0):
+            return -np.pi / 2
+
+        theta = np.arctan(displ_y/displ_x)
+        
+
+        if(displ_x > 0 and displ_y > 0): #body_alt is north east to self
+            return theta
+        elif(displ_x < 0 and displ_y > 0): #body_alt is north west to self
+            return np.pi - theta
+        elif(displ_x < 0 and displ_y < 0):  #body_alt is south west to self
+            return np.pi + theta
+        elif(displ_x > 0 and displ_y < 0): #body_alt is south east to self
+            return 2 * np.pi - theta
+        
+
+    def gravity(self, surface, body_alt):
+        """ Calculates then modifies self.acc_x, self.acc_y based on physics calculation """
+        displ_x = body_alt.pos_x - self.pos_x
+        displ_y = body_alt.pos_y - self.pos_y
+        displ = np.sqrt(displ_x ** 2 + displ_y ** 2)
+        
+        accel = (G * body_alt.mass) / (displ_x ** 2 + displ_y ** 2)
+        theta = self.__get_angle(body_alt, displ_x, displ_y)
+
+        if(self.mass == 10):
+            x_prime = (self.pos_x * np.cos(theta) - self.pos_y * np.sin(theta))
+            y_prime = (self.pos_x * np.sin(theta) + self.pos_y * np.cos(theta))
+            # pg.draw.line(surface, (255,0,0), (self.pos_x, self.pos_y), (x_prime, y_prime))
+            pg.draw.line(surface, (0,255,0), (self.pos_x, self.pos_y), (body_alt.pos_x, body_alt.pos_y))
+            pg.draw.line(surface, (0,255,0), (self.pos_x, self.pos_y), (self.pos_x, body_alt.pos_y))
+            pg.draw.line(surface, (0,255,0), (body_alt.pos_x, body_alt.pos_y), (self.pos_x, body_alt.pos_y))
+
+
+
+
+            print((theta * 180)/ np.pi)
+
+        self.acc_x += np.cos(theta) * accel
+        self.acc_y += np.sin(theta) * accel
+        
+
+
+        
         
     
 
